e3a8c367f8e119c7ddc7acbeef9f719fb9cb1384
Michael Newton <miken32@gmail.com>
2026-01-02 21:37:39 -0700

    remove jquery dependency

--- profiler/resources/profiler.js
+++ profiler/resources/profiler.js
@@ -1,91 +1,101 @@
-(function($) {
-    var profiler_details = false;
-    var height_toggle = false;
-    var selected_log_type = null;
+let profiler_details = false;
+let height_toggle = false;
+let selected_log_type = null;
 
-    function hideAllTabs() {
-        $('#profiler').removeClass('console')
-                 .removeClass('speed')
-                 .removeClass('queries')
-                 .removeClass('memory')
-                 .removeClass('files');
-    }
+function hideAllTabs() {
+    document.getElementById('profiler')
+        .classList
+        .remove('console', 'speed', 'queries', 'memory', 'files');
+}
 
-    $(document).ready(function() {
-        setTimeout(function() {
-            $('#profiler-container').css('display', 'block');
-        }, 10);
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        document.getElementById('profiler-container').style.display = 'block';
+    }, 10);
 
-        $('.query-profile H4').css('cursor', 'pointer').click(function() {
-            if ($('table', $(this).parent()).is(':hidden')) {
-                $(this).html('&#187; Hide Query Profile');
-                $('table', $(this).parent()).css('display', 'block');
+    document.querySelectorAll('.query-profile h4').forEach(function (el) {
+        el.style.cursor = 'pointer';
+        el.addEventListener('click', function () {
+            let table = this.parent.querySelector('table');
+            if (table && table.style.display === 'none') {
+                this.innerHTML = '&#187; Hide Query Profile';
+                table.style.display = 'block';
             } else {
-                $(this).html('&#187; Show Query Profile');
-                $('table', $(this).parent()).css('display', 'none');
+                this.innerHTML = '&#187; Show Query Profile';
+                table.style.display = 'none';
             }
         });
+    });
 
-        $('.detailsToggle').click(function() {
+    document.querySelectorAll('.detailsToggle').forEach(function (el) {
+        el.addEventListener('click', function () {
             if (profiler_details) {
-                $('#profiler-container').addClass('hideDetails');
+                document.getElementById('profiler-container').classList.add('hideDetails');
                 profiler_details = false;
             } else {
-                $('#profiler-container').removeClass('hideDetails');
+                document.getElementById('profiler-container').classList.remove('hideDetails');
                 profiler_details = true;
             }
 
             return false;
         });
+    });
 
-        $('.heightToggle').click(function() {
+    document.querySelectorAll('.heightToggle').forEach(function (el) {
+        el.addEventListener('click', function () {
             height_toggle = !height_toggle;
 
-            $('.profiler-box').each(function() {
-                $(this).css('height', (height_toggle ? '500px' : '200px'));
-            });
-        });
-
-        $('.tab').css('cursor', 'pointer').click(function() {
-            hideAllTabs();
-
-            $(this).addClass('active');
-            $('#profiler').addClass($(this).attr('id'));
-
-            if (!profiler_details) {
-                profiler_details = true;
-                $('#profiler-container').removeClass('hideDetails');
-            }
-        });
-
-        $('#profiler-console .side td').each(function() {
-            var log_type = $(this).attr('id').split('-')[1];
-            var log_count = $('var', $(this)).html();
-
-            if (log_count == 0) {
-                return;
-            }
-
-            $(this).css('cursor', 'pointer').click(function() {
-                $('#profiler-console .main tr').each(function() {
-                    var row_type = $(this).attr('class').split('-')[1];
-
-                    if (row_type == log_type || selected_log_type == log_type) {
-                        $(this).show();
-                    } else {
-                        $(this).hide();
-                    }
-                });
-
-                $('#profiler-console .side td').removeClass('selected');
-
-                if (selected_log_type == log_type) {
-                    selected_log_type = null;
-                } else {
-                    selected_log_type = log_type;
-                    $(this).addClass('selected');
-                }
+            document.querySelectorAll('.profiler-box').forEach(function (el) {
+                el.style.height = (height_toggle ? '500px' : '200px');
             });
         });
     });
-})(jQuery);
+
+    document.querySelectorAll('.tab').forEach(function(el) {
+        el.style.cursor = 'pointer';
+        el.addEventListener('click', function () {
+            hideAllTabs();
+
+            this.classList.add('active');
+            document.getElementById('profiler').classList.add(this.id);
+
+            if (!profiler_details) {
+                profiler_details = true;
+                document.getElementById('profiler-container').classList.remove('hideDetails');
+            }
+        });
+    });
+
+    document.querySelectorAll('#profiler-console .side td').forEach(function(el) {
+        var log_type = el.id.split('-')[1];
+        var log_count = el.querySelector('var')?.innerHTML;
+
+        if (log_count == 0) {
+            return;
+        }
+
+        el.style.cursor = 'pointer';
+        el.addEventListener('click', function() {
+            document.querySelectorAll('#profiler-console .main tr').forEach(function(el) {
+                const row_type = el.getAttribute("class").split('-')[1];
+
+                if (row_type === log_type || selected_log_type === log_type) {
+                    el.style.display = 'table-row';
+                } else {
+                    el.style.display = 'none';
+                }
+            });
+
+            document.querySelectorAll('#profiler-console .side td').forEach(function (el) {
+                el.classList.remove('selected');
+            });
+
+            if (selected_log_type === log_type) {
+                selected_log_type = null;
+            } else {
+                selected_log_type = log_type;
+                el.classList.add('selected');
+            }
+        });
+    });
+});
